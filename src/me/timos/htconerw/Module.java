03bb4582bbfc7495526a6ddac34ed43738ca89e2
thuanbkit <thuanbkit@gmail.com>
2013-09-04 05:10:34 +0000

    Add module to work with HTC One Sense 4.3 Version 1.0.11

---
+++ src/me/timos/htconerw/Module.java
@@ -0,0 +1,17 @@
+package me.timos.htconerw;
+
+public enum Module {
+	MOD41(R.raw.wp_mod_ko_41, 334, 15),
+	//
+	MOD43(R.raw.wp_mod_ko_43, 341, 16);
+
+	public final int RES_ID;
+	public final int OFFSET;
+	public final int LENGTH;
+
+	private Module(int resId, int offset, int length) {
+		RES_ID = resId;
+		OFFSET = offset;
+		LENGTH = length;
+	}
+}
